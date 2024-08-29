f616140b3e3b54bf1f2bd1cf5dc6af41b769c6a1
Muhammad Furqan <muhammadfurqan7427@gmail.com>
2024-08-29 18:39:26 +0700

    fix ui home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="icon" href="https://i0.wp.com/cdnimg.tcsc.my.id/2019/12/logo-qwords.jpg" type="image/jpg">
+  @vite('resources/css/app.css')
+  <title>Qwords.com: Cloud Web Hosting Indonesia Akses Cepat Aman</title>
+</head>
+<body>
+  <div class="bg-white">
+    <header class="absolute inset-x-0 top-0 z-50">
+      <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div class="flex lg:flex-1">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">Qwords</span>
+            <img class="h-10 w-auto" src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/logo-qw-light.webp" alt="">
+          </a>
+        </div>
+        <div class="flex lg:hidden">
+          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+            <span class="sr-only">Open main menu</span>
+            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
+        </div>
+        <div class="hidden lg:flex lg:gap-x-12">
+          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Cloud Hosting</a>
+          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Server</a>
+          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Domain</a>
+          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Email Suite</a>
+          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Services</a>
+          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Internet Access</a>
+        </div>
+        <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+          <span class="sm:ml-3">
+            <button type="button" class="border-2 border-orange-500 text-orange-500 bg-white hover:bg-orange-500 hover:text-white active:bg-orange-600 active:border-orange-600 transition-colors duration-300 px-4 py-2 rounded-2xl">
+            Log in
+            </button>
+          </span>
+        </div>
+      </nav>
+      <!-- Mobile menu, show/hide based on menu open state. -->
+      
+      </div>
+    </header>
+  
+    <div class="relative isolate px-6 pt-14 lg:px-8">
+     
+      <div class="mx-auto max-w-2xl py-32 sm:py-36 lg:py-46 bg-[url('https://qwords.com/v2/wp-content/themes/qwords/assets/images/shapes/webp/main-bg.webp')] ">
+        <div class="text-center p-6 rounded-lg"> <!-- Background overlay -->
+          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Cloud Hosting</h1>
+          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">untuk</h1>
+          <h1 class="text-4xl font-bold tracking-tight text-amber-600 sm:text-6xl">Kesuksesan Websitemu!</h1>
+          <p class="mt-6 text-lg leading-8 text-black">Qwords menawarkan layanan Cloud Hosting yang Cepat, Aman, dan Dukungan 24 jam untuk memenuhi kebutuhan hosting Anda. Tersertifikasi ISO 27001 dengan Data Center Tier 3.</p>
+          <div class="mt-10 flex items-center justify-center gap-x-6">
+            <p class="text-3xl font-semibold leading-6 text-amber-600">#QwordsYourSuccess <span aria-hidden="true"></span></p>
+          </div>
+          <div class="mt-10 flex items-center justify-center gap-x-6">
+            <a href="#" class="rounded-2xl bg-amber-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600">Pesan Sekarang</a>
+          </div>
+        </div>
+      </div>
+      
+      
+    </div>
+  </div>
+</body>
+</html>
